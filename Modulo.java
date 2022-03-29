4fd6a048cc9198981db1b6a1cf52b37a092bff6f
Alexandre Fraga <ahqfraga@hotmail.com>
2022-03-29 00:04:21 -0300

    subindo modulo

---
+++ Modulo.java
@@ -0,0 +1,22 @@
+public class Modulo {
+
+    public static void main(String[] args) {
+
+        double valor1 = 12.65;
+        System.out.println(valor1);
+
+        int valor2 = (int)valor1;
+        System.out.println(valor2);
+
+        long valor3 = (long)valor1;
+        System.out.println(valor3);
+
+        byte valor4 = (byte)valor1;
+        System.out.println(valor4);
+
+        float valor5 = (float)valor1;
+        System.out.println(valor5);
+        
+
+    }
+}
