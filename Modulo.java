d72e8686a9b8e1f48405d99b43978dc9a8a84318
Alexandre Fraga <ahqfraga@hotmail.com>
2022-04-01 16:46:28 -0300

    atualizando modulo

--- Modulo.java
+++ Modulo.java
@@ -2,21 +2,29 @@ public class Modulo {
 
     public static void main(String[] args) {
 
-        double valor1 = 12.65;
-        System.out.println(valor1);
+        Double dvalor1 = 12.79;
+        System.out.println(dvalor1);
 
-        int valor2 = (int)valor1;
-        System.out.println(valor2);
+        double valor1 = (double)dvalor1;
+        System.out.println(valor1);
 
         long valor3 = (long)valor1;
         System.out.println(valor3);
 
-        byte valor4 = (byte)valor1;
+        Byte valor2 = 60;
+        System.out.println(valor2);
+
+        byte valor4 = (byte)valor2;
         System.out.println(valor4);
 
         float valor5 = (float)valor1;
         System.out.println(valor5);
-        
+
+        Integer intnumero1 = 100;
+        System.out.println(intnumero1);
+
+        int valor6 = (int)intnumero1;
+        System.out.println(valor6);
 
     }
 }
